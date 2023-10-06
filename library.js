da22efc90836e654dd5f40c2a9b55b6facda7ce4
Conor Kay <conorkay1@gmail.com>
2023-10-05 23:49:51 -0400

    Change implementation of book display to table, adjust relevant methods.

--- library.js
+++ library.js
@@ -3,6 +3,7 @@ const addButton = document.getElementById("addButton");
 const newBookDialog = document.getElementById("newBookDialog");
 const newBookForm = document.getElementById("newBookForm");
 const cancelButton = document.getElementById("cancel");
+const table = document.getElementById("table");
 
 addButton.addEventListener("click", (event) => {
   newBookDialog.showModal();
@@ -33,36 +34,89 @@ cancelButton.addEventListener("click", (event) => {
   closeTheDialog(newBookDialog);
 });
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, index) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.index = index;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
+  var newIndex = myLibrary.length;
+  let newBook = new Book(title, author, pages, read, newIndex);
+  console.log(newBook.index);
 
   myLibrary.push(newBook);
   console.log(myLibrary);
-  displayLibrary();
+
+  //TODO: Make this into a separate function to avoid redundant code
+  let newRow = table.getElementsByTagName("tbody")[0].insertRow(-1);
+
+  for (i = 0; i < 5; ++i) {
+    let newCell = newRow.insertCell(-1);
+    var newText;
+
+    if (i === 0) {
+      newText = document.createTextNode(newBook.title);
+    } else if (i === 1) {
+      newText = document.createTextNode(newBook.author);
+    } else if (i === 2) {
+      newText = document.createTextNode(newBook.pages);
+    } else if (i === 3) {
+      var statusButton = document.createElement("button");
+      statusButton.type = "button";
+      statusButton.innerText = "Change Status";
+      newCell.appendChild(statusButton);
+      continue;
+    } else {
+      if (read) {
+        newText = document.createTextNode("Yes");
+      } else {
+        newText = document.createTextNode("No");
+      }
+    }
+    newCell.appendChild(newText);
+  }
 }
 
 function displayLibrary() {
-  clearBookshelf();
   myLibrary.forEach((Book) => {
-    let bookCard = document.createElement("div");
-    bookCard.className = "bookCard";
-    document.getElementById("bookshelf").appendChild(bookCard);
+    let newRow = table.insertRow(-1);
+
+    for (i = 0; i < 5; ++i) {
+      let newCell = newRow.insertCell(-1);
+      var newText;
+
+      if (i === 0) {
+        newText = document.createTextNode(Book.title);
+      } else if (i === 1) {
+        newText = document.createTextNode(Book.author);
+      } else if (i === 2) {
+        newText = document.createTextNode(Book.pages);
+      } else if (i === 3) {
+        var statusButton = document.createElement("button");
+        statusButton.type = "button";
+        statusButton.innerText = "Change Status";
+        newCell.appendChild(statusButton);
+        return;
+      } else {
+        if (read) {
+          newText = document.createTextNode("Yes");
+        } else {
+          newText = document.createTextNode("No");
+        }
+      }
+      newCell.appendChild(newText);
+    }
   });
 }
 
-function clearBookshelf() {
-  var bookshelf = document.getElementById("bookshelf");
-  var first = bookshelf.firstElementChild;
+function clearTable() {
+  var first = table.firstElementChild;
   while (first) {
     first.remove();
-    first = bookshelf.firstElementChild;
+    first = table.firstElementChild;
   }
 }
 
